69160197ab562fcc66755e8a99ebffd389406dfb
Muh Roshan <leoparmigiana@gmail.com>
2024-06-08 20:46:03 +0530

    Scrapped all the question from leetcode!

---
+++ Scrap/Questions_Scrap.py
@@ -0,0 +1,14 @@
+from leetscrape import GetQuestionsList
+import os
+
+def scrape_leetcode_to_csv():
+    """
+        Scrap all the questions
+    """
+    ls = GetQuestionsList()
+    ls.scrape()  # Scrape the list of questions
+    os.makedirs("../tests/data/")
+    ls.to_csv(directory="../tests/data/")
+    print("to csv done.")    
+    
+scrape_leetcode_to_csv()
